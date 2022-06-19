c23a694c0b613e546493f687f33473781f9151dc
vcarvalho0 <vitor1.santos00@gmail.com>
2022-06-19 19:54:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Aumentar</button>
+    </div>
+  )
+}
+export default App
